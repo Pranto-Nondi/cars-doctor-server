05db2b83ceba19e41dc1335dd3c3bd0ae397df29
Pranto-Nondie <pranto.cs.25@gmail.com>
2023-05-13 18:23:35 +0600

    prgress server

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-// cars-doctor
-// pU6PVS4EzRCLGMcJ
 
 
 
-const uri = "mongodb+srv://cars-doctor:pU6PVS4EzRCLGMcJ@cluster0.1eww0o2.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1eww0o2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
